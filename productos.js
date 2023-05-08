ccfbe8a586f0be5681b355a431bec38f9d3fd963
Natalia Rubio <nataliav.rubio@gmail.com>
2023-05-07 23:02:33 -0300

    Ultima Entrega

---
+++ productos.js
@@ -0,0 +1,66 @@
+const productos = [
+  {
+    id: 1,
+    imagen:
+      "https://amelipapeleria.uy/wp-content/uploads/2022/05/Cuaderno-A5-tapa-dura.-Colores-pasteles-estampados.-01.jpeg",
+    nombre: "Cuaderno",
+    descripcion: "Cuaderno con espiral y tapa dura.",
+    precio: 59,
+  },
+  {
+    id: 2,
+    imagen:
+      "https://cdn.shopify.com/s/files/1/0454/5557/3156/products/gomas3_1024x1024@2x.jpg?v=1602335820",
+    nombre: "Pack lápiz y goma",
+    descripcion: "Pack de 2 lápices y 2 gomas.",
+    precio: 70,
+  },
+  {
+    id: 3,
+    imagen:
+      "https://cdn.shopify.com/s/files/1/0454/5557/3156/products/Filgo_web_Markermedianopastel2_1024x1024@2x.jpg?v=1615389193",
+    nombre: "Marcadores",
+    descripcion: "Marcadores en colores pasteles.",
+    precio: 240,
+  },
+  {
+    id: 4,
+    imagen:
+      "https://cdn.shopify.com/s/files/1/0454/5557/3156/products/libretas1_360x.jpg?v=1613785256",
+    nombre: "Libreta",
+    descripcion: "Libreta de notas, tamaño mediano.",
+    precio: 120,
+  },
+  {
+    id: 5,
+    imagen:
+      "https://cdn.shopify.com/s/files/1/0454/5557/3156/products/lapiz1_360x.jpg?v=1607721444",
+    nombre: "Lápiz mecánico",
+    descripcion: "Lápiz mecánico, incluye grafos.",
+    precio: 50,
+  },
+  {
+    id: 6,
+    imagen:
+      "https://cdn.shopify.com/s/files/1/0454/5557/3156/products/WhatsAppImage2021-09-23at8.08.04PM_1_360x.jpg?v=1632438941",
+    nombre: "Clips",
+    descripcion: "Clips en caja. ",
+    precio: 100,
+  },
+  {
+    id: 7,
+    imagen:
+      "https://cdn.shopify.com/s/files/1/0454/5557/3156/products/WhatsAppImage2021-09-23at8.08.03PM_360x.jpg?v=1632439017",
+    nombre: "Pines",
+    descripcion: "Pines, varios tamaños y colores.",
+    precio: 98,
+  },
+  {
+    id: 8,
+    imagen:
+      "https://cdn.shopify.com/s/files/1/0454/5557/3156/products/diario1_360x.jpg?v=1597351290",
+    nombre: "Planner diario",
+    descripcion: "Planner diario, block de 50 hojas.",
+    precio: 275,
+  },
+];
